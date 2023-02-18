7abdeacfed5c131093a520da5f1bd6964fcfac77
Daqsoo <Mr.Spy710@gmail.com>
2023-02-18 14:29:15 +0200

    useState for add item to cart, useState for Categories, code refactoring

--- src/App.js
+++ src/App.js
@@ -7,27 +7,24 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzasList from './assets/pizzas.json';
+
 function App() {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
-              <PizzaBlock title="Mexican" price={13} />
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzasList.map((obj) => (
+                <PizzaBlock title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+              ))}
             </div>
           </div>
         </div>
