a2da06611aa5d5a98f0c655843f389f9820328e5
Daqsoo <Mr.Spy710@gmail.com>
2023-07-09 17:04:11 +0300

    added NotFound Component,fixed bugs

---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './NotFoundBlock.module.scss';
+
+export const NotFoundBlock = () => {
+  return (
+    <div>
+      <h1 className={styles.root}>Nothing Found &#129301; </h1>
+    </div>
+  );
+};
+export default NotFoundBlock;
